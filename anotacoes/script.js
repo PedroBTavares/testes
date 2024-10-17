f1a81b7b2045751bf5cc8df05dad07e7f30ae617
Pedro Bengozi Tavares <166571982+PedroBTavares@users.noreply.github.com>
2024-10-16 22:50:38 -0300

    adicionadas novas funções

--- anotacoes/script.js
+++ anotacoes/script.js
@@ -1,2 +1,36 @@
-const adArea = document.querySelector('#ad-area')
-adArea.style.display = 'none'
+const cxAdArea = document.querySelector('#cx-ad-area')
+cxAdArea.style.display = 'none'
+const areas = document.querySelector('#areas')
+let nomeArea = ''
+const aviso = document.querySelector('#aviso')
+
+function mostrarAdArea(){
+    cxAdArea.style.display = 'block'
+}
+function adArea(){
+    const titulo = document.querySelector('#titulo')
+    nomeArea = titulo.value
+    const area = document.createElement('button')
+    area.classList.add('area')
+    areas.appendChild(area)
+    area.innerText = titulo.value
+    area.addEventListener('click', exArea)
+
+    cxAdArea.style.display = 'none'
+    titulo.value = ''
+}
+function escAdArea(){
+    cxAdArea.style.display = 'none'
+    const titulo = document.querySelector('#titulo')
+    titulo.value = ''
+}
+
+function exArea(){
+    aviso.style.display = 'none'
+
+    const tituloArea = document.querySelector('#titulo-area')
+    tituloArea.innerText = nomeArea
+
+    const liFixo = document.querySelector('#li-fixo')
+    liFixo.style.display = 'block'
+}
